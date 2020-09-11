d91f7c98cd7d6590bb66fd63cafaaed5a3a6762b
Nathaniel Lee <nateshot@Nathaniels-MacBook-Pro.local>
2020-09-10 17:51:06 -0700

    nl - implementing navigation system

--- App.js
+++ App.js
@@ -1,13 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
+import React from 'react';
+import 'react-native-gesture-handler';
+import {NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import { StyleSheet, Text, Image, View, SafeAreaView, TouchableHighlight, Button, Alert, Platform, StatusBar } from 'react-native';
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style = {{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button onPress = {() => navigation.navigate("Details") }title = "go to details screen"/>
+    </View>
+  )
+}
+const DetailsScreen = ({navigation}) => {
+  return (
+  <View style = {{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Details Screen</Text>
+        <Button onPress = {() => navigation.push("Details") }title = "go to details screen again"/>
+        <Button onPress = {() => navigation.navigate("Home") }title = "go home"/>
+        <Button onPress = {() => navigation.goBack() }title = "go back"/>
+        <Button onPress = {() => navigation.popToTop() }title = "go first screen"/>
+      </View>
+  )
+}
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Homemade</Text>
-      <StatusBar style="auto" />
-    </View>
+   
+    <NavigationContainer>
+      <Stack.Navigator screenOptions = {{
+          headerStyle: {
+            backgroundColor: "#009387",
+          },
+          headerTintColor: "#ffffff",
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          }
+      }}>
+        <Stack.Screen name = "Home" component = {HomeScreen} options = {{
+          
+        }}/>
+        <Stack.Screen name = "Details" component = {DetailsScreen}/>
+        
+      </Stack.Navigator>
+      </NavigationContainer>
+
   );
 }
 
@@ -17,5 +55,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Platform.OS == "android" ? StatusBar.currentHeight : 0
   },
 });
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { ImageBackground, StyleSheet, View } from 'react-native';
+
+function WelcomeScreen (props) {
+    return (
+        <ImageBackground 
+        style = {styles.background}
+        source = {{uri: "https://picsum.photos/id/870/200/300?grayscale&blur=2"}}>
+        <View style = {styles.loginButton}>
+
+        </View>
+        <View style = {styles.registerButton}>
+
+        </View>
+        </ImageBackground>
+    )
+}
+
+const styles = StyleSheet.create ({
+    background: {
+        flex: 1,
+        justifyContent: "flex-end",
+
+    },
+    loginButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: "#fc5c65",
+    },
+    registerButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: "#4ecdc4",
+    }
+});
+
+export default WelcomeScreen;
